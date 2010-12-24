0577f74fcdc861cbbb76e18e509c87d18c31953a
Ken Collins <ken@metaskills.net>
2010-12-23 23:02:43 -0500

    Adding several more posts.

--- resource/site.js
+++ resource/site.js
@@ -18,44 +18,56 @@ _gaq.push(['_trackPageview']);
 })();
 
 
+// Persnikity Pygmentize
+
+var MetaPygmentize = {
+  
+  process: function() {
+    this.convert('nb', 'n',   ['id','name']);
+    this.convert('nb', 'c1',  ['self']);
+    this.convert('n',  'nb',  ['new']);
+    this.convert('o',  'k',   ['*','&','+','/','=']);
+    this.convert('nx', 'k',   ['$','$$']);
+    this.convert('k',  'kjs', ['this']);
+  },
+  
+  arrayInclude: function(array, obj) {
+    var i = array.length;
+    while (i--) {
+      if (array[i] === obj) { return true; }
+    }
+    return false;
+  },
+  
+  convert: function(fromClass, toClass, texts) {
+    var spans = $('div.highlight span.' + fromClass);
+    console.log(spans.get());
+    spans.each(function(s){
+      var span = $(s);
+      var text = span.text();
+      var target = MetaPygmentize.arrayInclude(texts,text);
+      if (target) { span.attr('class',toClass); };
+    });
+  }
+  
+}
+
 // My Namespace
+
 var MetaSkills = {
   
   appendContentForAppleTvNavigation: function() {
     $('#page nav a').append('<span></span>');
-  },
-  
-  bePersnikityWithPygmentize: function() {
-    $('div.highlight span.nb').each(function(s){
-      var span = $(s);
-      switch (span.text()) {
-        case 'id':
-        case 'name':
-          span.attr('class','n');
-          break;
-        default:
-          break;
-      }
-    });
-    $('div.highlight span.n').each(function(s){
-      var span = $(s);
-      switch (span.text()) {
-        case 'new':
-          span.attr('class','nb');
-          break;
-        default:
-          break;
-      }
-    });
   }
   
 };
 
 
 // Document Loads
+
 $(document).ready(function(){
   MetaSkills.appendContentForAppleTvNavigation();
-  MetaSkills.bePersnikityWithPygmentize();
+  MetaPygmentize.process();
 })
 
 
